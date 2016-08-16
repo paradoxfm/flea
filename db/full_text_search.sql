d239ca28056e9c87e86ec1415c60683fa4419a8e
paradoxfm <paradoxfm@mail.ru>
2016-08-17 01:53:18 +0500

    правка

--- db/full_text_search.sql
+++ db/full_text_search.sql
@@ -2,14 +2,14 @@
 -- словари из папки dict надо скопировать в C:/Program Files (x86)/PostgreSQL/9.5/share/tsearch_data/
 -- stop words там уже есть, но на всякий случай в папке есть для русского
 create text search dictionary ispell_ru (
-template  =   ispell,
+	template  =   ispell,
 	dictfile  =   ru,
 	afffile   =   ru,
 	stopwords =   russian
 );
 
 create text search dictionary ispell_en (
-template  = ispell,
+	template  = ispell,
 	dictfile  = en,
 	afffile   = en,
 	stopwords = english
@@ -17,15 +17,11 @@ template  = ispell,
 
 create text search configuration ru ( copy = russian );
 
-alter text search configuration ru
-alter mapping
-for word, hword, hword_part
-with ispell_ru, russian_stem;
+alter text search configuration ru alter mapping
+for word, hword, hword_part with ispell_ru, russian_stem;
 
-alter text search configuration ru
-alter mapping
-for asciiword, asciihword, hword_asciipart
-with ispell_en, english_stem;
+alter text search configuration ru alter mapping
+for asciiword, asciihword, hword_asciipart with ispell_en, english_stem;
 
 --set default_text_search_config = 'ru';
 -- в postgresql.conf default_text_search_config = 'ru'
